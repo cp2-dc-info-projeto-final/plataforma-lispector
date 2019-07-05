f463fc3045d800e12547baa16f708ca87177990a
AnaBeatrizFerreira <42150172+AnaBeatrizFerreira@users.noreply.github.com>
2019-07-05 09:12:48 -0300

    atualização cadastro de professores

--- src/cadastro_professor.php
+++ src/cadastro_professor.php
@@ -1,11 +1,10 @@
 <?php
+$id = $_POST['id'];
 $nome = $_POST['nome'];
-$senha = $_POST['senha']; // aumentar campo  no banco para usar o MD5
-//$senha = MD5($_POST['senha']);
-$Email = $_POST['email'];
+$email = $_POST['email'];
 $matricula = $_POST['matricula'];
-
-$link = mysqli_connect("localhost", "root", "", "professor2");
+$senha = MD5($_POST['senha']);
+$link = mysqli_connect("localhost", "root", "", "plataforma_lispector");
  
 // Check connection
 if($link === false){
@@ -13,12 +12,11 @@ if($link === false){
 }
  
 // Attempt insert query execution
-$sql = "INSERT INTO professor(nome,matricula,email,senha) VALUES ('$nome', '$matricula', '$Email', '$senha')";
+$sql = "INSERT INTO professor(id, nome, matricula, email, senha) VALUES ('$id', '$nome', '$matricula', '$email', '$senha')";
 if(mysqli_query($link, $sql)){
     echo "DADOS INSERIDOS COM SUCESSO.";
 } else{
     echo "ERRO: NÃO FOI POSSÍVEL CONECTAR AO $sql." . mysqli_error($link);
 }
-
 mysqli_close($link);
 ?>
