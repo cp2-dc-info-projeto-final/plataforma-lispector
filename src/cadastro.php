28164c9c6fe7e959e4bf70498a860f34db8235b3
denisIN <43256327+denisIN@users.noreply.github.com>
2019-07-08 09:43:16 -0300

    adicionando arquivos php da aula 2

--- src/cadastro.php
+++ src/cadastro.php
@@ -1,5 +1,43 @@
 <?php
-
-incluide_once('/connectbd.php');
-
-$nome
+    $id = $_POST["id"];
+    $nome = $_POST["nome"];
+    $email = $_POST["email"];
+    $matricula = $_POST["matricula"];
+    $senha = MD5$_POST["senha"];
+    $confirmaSenha = $_POST["confirmaSenha"];
+    session_start();
+    if ($senha != $confirmaSenha) {    
+        $erro = "As senhas não coincidem";        
+        $_SESSION["erro"] = $erro;
+        header("Location: formCadastro.php");
+        exit();
+    }c
+    # password hash
+    $hash = password_hash($senha, PASSWORD_DEFAULT);
+    $connection = mysqli_connect("localhost", "root", "", "plataforma_lispector");
+ 
+    // Check connection
+    if($connection === false){
+        die("Deu ruim, mano!" . mysqli_connect_error());
+    }
+    $sql = "SELECT id FROM usuario WHERE email='$email'";
+    $result = mysqli_query($connection, $sql);
+    $erro = "";
+    
+    if (mysqli_num_rows($result) > 0) {
+        $erro = "E-mail indisponível.";        
+        $_SESSION["erro"] = $erro;
+        header("Location: formCadastro.php");
+        exit();
+    }
+    $sql = "INSERT INTO usuario (id, nome, email, matricula, senha, confirmaSenha) VALUES
+            ('$id', '$nome', '$email', '$matricula', '$senha', '$hash')";
+    if(mysqli_query($connection, $sql)){
+        session_unset();
+        header("Location: formLogin.php");
+        exit();
+    } else{
+        die("Deu ruim no cadastro $sql. " . mysqli_error($connection));
+    }
+    mysqli_close($connection);
+?>
---
+++ src/formLogin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Login</title>
+</head>
+<body>
+<h1>Login</h1>
+<form action="login.php" method="post">
+    <p>
+        <label for="email">Email</label>
+        <input type="text" name="email" id="email">
+    </p>
+    <p>
+        <label for="senha">Senha</label>
+        <input type="password" name="senha" id="senha">
+    </p>
+    <input type="submit" value="Enviar">
+    <?php
+        session_start();
+        if(array_key_exists('erro', $_SESSION) == true){
+            $erro = $_SESSION["erro"];
+            echo "<br><b>$erro</b>";
+            session_unset();
+        }
+    ?>
+    <p>
+        <a href="formCadastro.php">Cadastre-se</a>
+    </p>
+</form>
+</body>
+</html>
