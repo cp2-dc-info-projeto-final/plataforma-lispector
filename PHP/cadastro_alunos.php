3a8bfe6764b5dfd896a70bc2e309297a3db4a35e
denisIN <denis.pai.mommy@gmail.com>
2019-08-28 16:31:43 -0300

    inserção de dados, com exceção do confirmaSenha, funcionando

--- PHP/cadastro_alunos.php
+++ PHP/cadastro_alunos.php
@@ -3,23 +3,21 @@ $nome = $_POST['nome'];
 $email = $_POST['email'];
 $matricula = $_POST['matricula'];
 $turma = $_POST['turma'];
-$senha = ($_POST['senha']);
-$confirmaSenha = ($_POST["confirmaSenha"]);
+$senha = $_POST['senha'];
+$confirmaSenha = $_POST["confirmaSenha"];
+
 session_start();
-if ($senha != $confirmaSenha) {    
-    $erro = "SENHAS NÃO COINCIDENTES";        
-    $_SESSION["erro"] = $erro;
-    header("Location: ../HTML/formCadastrodeAlunos.php");
-    exit();
-}
+
 # password hash
 $hash = password_hash($senha, PASSWORD_DEFAULT);
+
 $connection = mysqli_connect("localhost", "root", "", "plataforma_lispector");
 
 // Check connection
 if($connection === false){
     die("Deu ruim, mano!" . mysqli_connect_error());
 }
+
 $sql = "SELECT id FROM alunos WHERE email='$email'";
 $result = mysqli_query($connection, $sql);
 $erro = "";
@@ -27,15 +25,15 @@ $erro = "";
 if (mysqli_num_rows($result) > 0) {
     $erro = "E-mail indisponível.";        
     $_SESSION["erro"] = $erro;
-    header("Location: CadastrodeAlunos.php");
+    header("Location: ../HTML/formCadastrodeAlunos.php");
     exit();
 }
 // Attempt insert query execution
-$sql = "INSERT INTO alunos (nome, email, matricula, turma, senha, confirmaSenha) VALUES ('$nome', '$email', '$matricula', '$turma', '$senha', '$hash')";
+$sql = "INSERT INTO alunos (nome, email, matricula, turma, senha) VALUES ('$nome', '$email', '$matricula', '$turma', '$hash')";
 if(mysqli_query($connection, $sql)){
     echo "DADOS INSERIDOS COM SUCESSO.";
 } else{
-    echo "ERRO: NÃO FOI POSSÍVEL CONECTAR AO $sql." . mysqli_error($link);
+    echo "ERRO: NÃO FOI POSSÍVEL CONECTAR AO $sql.";
 }
 mysqli_close($connection);
 ?>
