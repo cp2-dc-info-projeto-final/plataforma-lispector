881438c350fb88758accd560ccfe4e82f48a3333
denisIN <denis.pai.mommy@gmail.com>
2019-08-30 09:01:46 -0300

    tentando fazer funcionar o cadastro de professores

--- PHP/cadastro_professor.php
+++ PHP/cadastro_professor.php
@@ -1,22 +1,38 @@
 <?php
-
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $matricula = $_POST['matricula'];
-$senha = MD5($_POST['senha']);
-$link = mysqli_connect("localhost", "root", "", "plataforma_lispector");
- 
+$senha = $_POST['senha'];
+$confirmaSenha = $_POST["confirmaSenha"];
+
+session_start();
+
+# password hash
+$hash = password_hash($senha, PASSWORD_DEFAULT);
+
+$connection = mysqli_connect("localhost", "root", "", "plataforma_lispector");
+
 // Check connection
-if($link === false){
-    die("ERRO: NÃO FOI POSSÍVEL CONECTAR." . mysqli_connect_error());
+if($connection === false){
+    die("Deu ruim, mano!" . mysqli_connect_error());
 }
+
+$sql = "SELECT id FROM professor WHERE email='$email'";
+$result = mysqli_query($connection, $sql);
+$erro = "";
  
+if (mysqli_num_rows($result) > 0) {
+    $erro = "E-mail indisponível.";        
+    $_SESSION["erro"] = $erro;
+    header("Location: ../HTML/formCadastrodeAlunos.php");
+    exit();
+}
 // Attempt insert query execution
-$sql = "INSERT INTO professor(nome, matricula, email, senha) VALUES ('$nome', '$matricula', '$email', '$senha')";
-if(mysqli_query($link, $sql)){
+$sql = "INSERT INTO professor (nome, email, matricula, senha) VALUES ('$nome', '$email', '$matricula', '$hash')";
+if(mysqli_query($connection, $sql)){
     echo "DADOS INSERIDOS COM SUCESSO.";
 } else{
-    echo "ERRO: NÃO FOI POSSÍVEL CONECTAR AO $sql." . mysqli_error($link);
+    echo "ERRO: NÃO FOI POSSÍVEL CONECTAR AO $sql.";
 }
-mysqli_close($link);
+mysqli_close($connection);
 ?>
