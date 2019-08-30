335a874862dc72a59cec8a61885b6ae8ebca0f48
denisIN <denis.pai.mommy@gmail.com>
2019-08-30 10:42:37 -0300

    CORREÇÕES DIVERSAS

--- PHP/cadastro_alunos.php
+++ PHP/cadastro_alunos.php
@@ -25,7 +25,7 @@ $erro = "";
 if (mysqli_num_rows($result) > 0) {
     $erro = "E-mail indisponível.";        
     $_SESSION["erro"] = $erro;
-    header("Location: ../HTML/formCadastrodeAlunos.php");
+    header("Location: ../PHP/formCadastrodeAlunos.php");
     exit();
 }
 // Attempt insert query execution
