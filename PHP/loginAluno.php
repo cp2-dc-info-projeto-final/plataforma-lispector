ca4e8cad95efbf288634496211984066f26231cc
Lab Caxias <samara.paiva259@gmail.com>
2019-09-25 16:56:21 -0300

    concertando bug confirmar senha

--- PHP/loginAluno.php
+++ PHP/loginAluno.php
@@ -8,7 +8,7 @@
             die("Erro de Conecção" . mysqli_connect_error());
         }
         
-        $sql = "SELECT senha,nome FROM aluno WHERE email='$email'";
+        $sql = "SELECT senha,nome FROM professor WHERE email='$email'";
     
         $result = mysqli_query($connection, $sql);
         
