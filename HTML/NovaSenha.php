96cd2dac8152714cd4e5d6e773999a957196f77b
AlunaKeth <ke.silvacosta@gmail.com>
2019-10-07 09:46:32 -0300

    !!!!!!

---
+++ HTML/NovaSenha.php
@@ -0,0 +1,18 @@
+<?php>
+
+include("conexao.php");
+
+if(isset($_POST[ok])){
+
+    $novasenha = substr(md5(time()), 0, 6);
+
+}
+
+echo substr(md5(time()), 0, 6);
+
+?>
+
+<form action="">
+    <input placeholder= "Seu e-mail" name="email" type="text">
+    <input name="ok" value="ok" type="submit">
+</form>
