7bca61540261d1e4fd51e8edeed8ca6e159b4b29
Lab Caxias <cp2dcds@outlook.com>
2019-10-21 08:41:11 -0300

    correção de sintaxe

--- PHP/envio.php
+++ PHP/envio.php
@@ -28,7 +28,7 @@ $uploadir='../var/www/uploads';
 $uploadfile=$uploadir.basename($_FILES['userfile']['name']);
 echo '<pre>';
 if(move_uploaded_file($_FILES['userfile']['tmp_name'],$uploadfile)){
-    echo "Arquivo enviado !";
+    echo "Arquivo enviado!";
 }else{
     echo "Não foi possível enviar o arquivo";
 }
