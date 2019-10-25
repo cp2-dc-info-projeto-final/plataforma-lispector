f495fbb636dbfb3931fffc8451b3fd03ff5d5b29
Samaraa <samara.paiva259@gmail.com>
2019-10-25 09:21:12 -0300

    Add files via upload

--- Banco de Dados/plataforma_lispector.sql
+++ Banco de Dados/plataforma_lispector.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 16-Ago-2019 às 14:23
+-- Generation Time: 25-Out-2019 às 13:56
 -- Versão do servidor: 5.7.17
 -- PHP Version: 5.6.30
 
@@ -51,7 +51,22 @@ INSERT INTO `alunos` (`id`, `nome`, `email`, `senha`, `matricula`, `turma`) VALU
 (1231231231, 'aaaaaaaaa', 'aaaaa@eeem.com', '12312333', '111212222', NULL),
 (2131233, 'aaassss', 'asasass@asas.com', '852674', '33343434', 666),
 (777, 'tyyt', 'tyyt@tyyt.com', 'f35c65d67d49610b664ae6b25324007e', '4444', 123),
-(123456789, 'aaaaaaaaa', 'wrqwre3r@bol.com.br', '657f8b8da628ef83cf69101b6817150a', '1234568780', 1203);
+(123456789, 'aaaaaaaaa', 'wrqwre3r@bol.com.br', '657f8b8da628ef83cf69101b6817150a', '1234568780', 1203),
+(1231231232, 'Anbi', 'anbiferreira@gmal.com', '$2y$10$JBvLPtmCncA8mAOd5WyRV.6wvdBbjw1G7NpqeEL.wQe1rlUzG0cDu', 'm0300098', 1002),
+(1231231233, 'Anbi', 'aanbi@gmail.com', '$2y$10$o/7LUSOd7NsPxvFvyVJGCOQ94cbSUWorbSdH5VNejdlsfrl/PUWB.', 'm0300098', 1002);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionario`
+--
+
+CREATE TABLE `funcionario` (
+  `id` int(60) NOT NULL,
+  `nome` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `senha` varchar(60) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -74,7 +89,11 @@ CREATE TABLE `professor` (
 INSERT INTO `professor` (`id`, `nome`, `email`, `matricula`, `senha`) VALUES
 (1234, 'sdaad', 'safaadwe @ef.com', 'se4w34535', 'b8f34f0c25b3f5747b1999efc33b51c5'),
 (12345, 'sdgfseg', 'fdgsgzsr@g.com', '12345656', '37e0c86903875b5673b5cd4b03fe8082'),
-(12346, 'dsfsf', 'asdwdfwqf#@g.vidfv', '1111111111111111', 'a2c18fb090ffa411fa2058215e3dab44');
+(12346, 'dsfsf', 'asdwdfwqf#@g.vidfv', '1111111111111111', 'a2c18fb090ffa411fa2058215e3dab44'),
+(12347, 'Anbi', 'anbi@gmail.com', '02700349', '$2y$10$EiP6XD/QipSndxYWxFlzwuJUHao/yNOEJe6MnmOJp2GAOrxcB68u6'),
+(12348, '', '', '', '$2y$10$cgDhTq6rxNOXojpX05AmUeU6erKfsww57EZP3Dq2rp0FK3j2ZH6eG'),
+(12349, 'Anbi', 'theanbi@gmail.com', 'm0300098', '$2y$10$4q8qTB9IBNBerX7JgSISsOxXaJuZKPkfyG2P0nKqaCwZHMA7EIgpO'),
+(12350, 'denis', 'denis.pai.mommy@gmail.com', '02700349', '$2y$10$B7MLZe/sBVwSBWZXL1A6q.eQFnDU0LDz3CQ.sj0hvOA8FE7sh7CQe');
 
 --
 -- Indexes for dumped tables
@@ -84,6 +103,12 @@ INSERT INTO `professor` (`id`, `nome`, `email`, `matricula`, `senha`) VALUES
 -- Indexes for table `alunos`
 --
 ALTER TABLE `alunos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `funcionario`
+--
+ALTER TABLE `funcionario`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -100,12 +125,17 @@ ALTER TABLE `professor`
 -- AUTO_INCREMENT for table `alunos`
 --
 ALTER TABLE `alunos`
-  MODIFY `id` int(60) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1231231232;
+  MODIFY `id` int(60) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1231231234;
+--
+-- AUTO_INCREMENT for table `funcionario`
+--
+ALTER TABLE `funcionario`
+  MODIFY `id` int(60) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `professor`
 --
 ALTER TABLE `professor`
-  MODIFY `id` int(60) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12347;COMMIT;
+  MODIFY `id` int(60) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12351;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
