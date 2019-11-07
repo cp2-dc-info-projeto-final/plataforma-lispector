912fb7c1c7ade69caafe39afcc6bf4d61cd94777
AlunaKeth <ke.silvacosta@gmail.com>
2019-11-07 16:24:54 -0300

    Samara deixou aqui!

--- PHP/formCadastrodeAlunos.php
+++ PHP/formCadastrodeAlunos.php
@@ -5,22 +5,20 @@
 <link rel="stylesheet" href="../CSS/Menu.css">
 
 <ul>
-			<li><a href="../HTML/Index.html">Home</a></li>
+<li><a href="../HTML/Index.html">Home</a></li>
 	     <li><a href="formCadastrodeFuncionarios.php">Cadastros</a>
           <ul>
 					<li><a href="formCadastrodeFuncionarios.php">Cadastro de Funcionarios</a></li>
           <li><a href="formCadastrodeProfessores.php">Cadastro de Professores</a></li>
           <li><a href="formCadastrodeAlunos.php">Cadastro de Alunos</a><li>
 </ul>        
-			<li><a href="#">Logins</a>
-				<ul>
-					<li><a href="../PHP/frmLoginAluno.php">Login do Alunos</li>
-						<li><a href="../PHP/frmLoginProfessor.php">Login do Professor</a></li>
-            <li><a href="../PHP/frmLoginfuncionario.php">Login do Funcionario</a></li>
-        </ul>
-        </li>
-			<li><a href="PáginaDeEnvio.html">Área do Professor</a>
-			</li>
+<li><a href="#">Logins</a>
+          <ul>
+					<li><a href="frmLoginAluno.php">Login do Aluno</a></li>
+          <li><a href="frmLoginProfessor.php">Login do Professor</a></li>
+          <li><a href="frmLoginfuncionario.php">Login do Funcionário</a><li>
+</ul>      
+<li><a href="PáginaDeEnvio.html">Área do Professor</a></li>
 
     </ul>
     
