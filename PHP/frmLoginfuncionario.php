8093edac6bb7bacf8fbec8397ee2cae4cfaf1a11
AlunaKeth <ke.silvacosta@gmail.com>
2019-11-15 15:12:08 -0200

    Menus Linkados!

--- PHP/frmLoginfuncionario.php
+++ PHP/frmLoginfuncionario.php
@@ -3,7 +3,29 @@
 <script type="text/javascript" src="../JavaScript/Logins.js"> </script>
 <div id="login">
 
-  
+<nav  class="menu">
+
+<link rel="stylesheet" href="../CSS/Menu.css">
+
+<ul>
+<li><a href="../HTML/Index.html">Home</a></li>
+	     <li><a href="formCadastrodeFuncionarios.php">Cadastros</a>
+          <ul>
+					<li><a href="formCadastrodeFuncionarios.php">Cadastro de Funcionarios</a></li>
+          <li><a href="formCadastrodeProfessores.php">Cadastro de Professores</a></li>
+          <li><a href="formCadastrodeAlunos.php">Cadastro de Alunos</a><li>
+</ul>    
+<li><a href="#">Logins</a>
+          <ul>
+					<li><a href="frmLoginAluno.php">Login do Aluno</a></li>
+          <li><a href="frmLoginProfessor.php">Login do Professor</a></li>
+          <li><a href="frmLoginfuncionario.php">Login do Funcionário</a><li>
+</ul>      
+<li><a href="PáginaDeEnvio.html">Área do Professor</a></li>
+
+    </ul>
+    
+  </nav>
         <div class="login-page">
 
 <div class="form">
