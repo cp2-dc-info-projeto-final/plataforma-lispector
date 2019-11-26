ffe51cdc0138a839d13ccdedfdeb2570eb465132
AlunaKeth <ke.silvacosta@gmail.com>
2019-11-26 17:26:39 -0300

    Coloquei Menu nos Cadastros!

--- PHP/formCadastrodeAlunos.php
+++ PHP/formCadastrodeAlunos.php
@@ -1,91 +1,64 @@
-<!DOCTYPE html>
+<link rel="stylesheet" href="../CSS/Logins.css" >
 
-<head>
+<script type="text/javascript" src="../JavaScript/Logins.js"> </script>
+<div id="login">
 
-  <meta charset="UTF-8" />
+<nav  class="menu">
 
-  <title>Cadastro de Alunos</title>
+<link rel="stylesheet" href="../CSS/Menu.css">
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-
-  <link rel="stylesheet" href="../CSS/Teste.css">
-
-<!-- MENU!--> 
-  <ul>
-
+<ul>
 <li><a href="../HTML/Index.html">Home</a></li>
-	     <li><a href="formCadastrodeFuncionarios.php">Cadastros</a>
+<li><a href="formCadastrodeFuncionarios.php">Cadastros</a>
           <ul>
 					<li><a href="formCadastrodeFuncionarios.php">Cadastro de Funcionarios</a></li>
           <li><a href="formCadastrodeProfessores.php">Cadastro de Professores</a></li>
           <li><a href="formCadastrodeAlunos.php">Cadastro de Alunos</a><li>
-</ul>    
+</ul>
 <li><a href="#">Logins</a>
           <ul>
 					<li><a href="frmLogindeAlunos.php">Login do Aluno</a></li>
           <li><a href="frmLoginProfessor.php">Login do Professor</a></li>
           <li><a href="frmLoginfuncionario.php">Login do Funcionário</a><li>
-</ul>      
 
-
-    </ul>
     
   </nav>
 
-<!--FINAL DO MENU!--> 
-
-</head>
-
-<body>
-
-      <div id="cadastro">
-
-        <form method="post" action="codigo_login_aluno.php"> 
-        
-          <h1>Cadastro de Alunos</h1> 
-
-          <p> 
-            <label for="nome_cad">Digite seu nome</label>
-            <input id="nome_cad" name="nome_cad" required="required" type="text" placeholder="nome" />
-          </p>
-
-          <p> 
-            <label for="email_cad">Digite Seu e-mail</label>
-            <input id="email_cad" name="email_cad" required="required" type="email" placeholder="contato@htmlecsspro.com"/> 
-          </p>
+  
+        <div class="login-page">
           
-          <p> 
-            <label for="matricula_cad">Digite seu Matrícula</label>
-            <input id="matricula_cad" name="matricula_cad" required="required" type="text" placeholder="ex:M02100361" />
-          </p>
 
-            <label for="turma_cad">Digite sua turma</label>
-            <input id="turma_cad" name="turma_cad" required="required" type="text" placeholder="ex:1103" />
-          </p>
+<div class="form">
 
-          <p> 
-            <label for="senha_cad">Crie uma senha</label>
-            <input id="senha_cad" name="senha_cad" required="required" type="password" placeholder="ex: 1234"/>
-          </p>
+  <form class="register-form">
 
-          <p> 
-              <label for="senha_cad1">Confirme sua senha</label>
-              <input id="senha_cad1" name="senha_cad1" required="required" type="password" placeholder="ex: 1234"/>
-            </p>
-            
-          <p> 
-            <input type="submit" value="Cadastrar"/> 
-          </p>
-           
-          <p class="link">  
-            Já tem conta?
-            <a href="formLogindeAlunos.php">Entre!</a>
-          </p>
-        </form>
+  <form method="post" action="codigo_login_profesor.php"> 
 
-       
-      </div>
-    </div>
-  </div>  
-</body>
-</html>
+  </form>
+
+  <form class="login-form">
+
+  <h1> Cadastro de Professores </h1>
+
+    <input type="name" id="name" name="name" placeholder="Digite seu Nome"/>
+
+    <input type="turma" id="email" name="turma" placeholder="Digite sua Turma:"/>
+
+    <input type="matricula" id="email" name="matricula" placeholder="Digite sua Matrícula:"/>
+
+    <input type="email" id="email" name="email" placeholder="Digite seu E-mail"/>
+
+    <input type="password" id="email" name="senha" placeholder="Crie uma Senha"/>
+
+    <input type="password" id="email" name="senha" placeholder="Confirme sua Senha"/>
+
+
+    <input type="submit" value="Cadastrar"/>
+
+    <p class="message">Já é cadastrado? <a href="frmLogindeAlunos.php">Entre!</a></p>
+
+  </form>
+
+</div>
+
+</div>
--- PHP/formCadastrodeProfessores.php
+++ PHP/formCadastrodeProfessores.php
@@ -1,60 +1,60 @@
-<!DOCTYPE html>
+<link rel="stylesheet" href="../CSS/Logins.css" >
 
-<head>
+<script type="text/javascript" src="../JavaScript/Logins.js"> </script>
+<div id="login">
 
-  <meta charset="UTF-8" />
+<nav  class="menu">
 
-  <title>Cadastro de Professores</title>
+<link rel="stylesheet" href="../CSS/Menu.css">
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<ul>
+<li><a href="../HTML/Index.html">Home</a></li>
+<li><a href="formCadastrodeFuncionarios.php">Cadastros</a>
+          <ul>
+					<li><a href="formCadastrodeFuncionarios.php">Cadastro de Funcionarios</a></li>
+          <li><a href="formCadastrodeProfessores.php">Cadastro de Professores</a></li>
+          <li><a href="formCadastrodeAlunos.php">Cadastro de Alunos</a><li>
+</ul>
+<li><a href="#">Logins</a>
+          <ul>
+					<li><a href="frmLogindeAlunos.php">Login do Aluno</a></li>
+          <li><a href="frmLoginProfessor.php">Login do Professor</a></li>
+          <li><a href="frmLoginfuncionario.php">Login do Funcionário</a><li>
 
-  <link rel="stylesheet" href="../CSS/Teste.css">
+    
+  </nav>
+
+  
+        <div class="login-page">
+          
+
+<div class="form">
+
+  <form class="register-form">
+
+  <form method="post" action="codigo_login_profesor.php"> 
+
+  </form>
+
+  <form class="login-form">
+
+  <h1> Cadastro de Professores </h1>
+
+    <input type="name" id="name" name="name" placeholder="Digite seu Nome"/>
+
+    <input type="email" id="email" name="email" placeholder="Digite seu E-mail"/>
+
+    <input type="password" id="email" name="senha" placeholder="Crie uma Senha"/>
+
+    <input type="password" id="email" name="senha" placeholder="Confirme sua Senha"/>
 
 
-</head>
+    <input type="submit" value="Cadastrar"/>
 
-<body>
+    <p class="message">Já é cadastrado? <a href="frmLogindeProfessor.php">Entre!</a></p>
 
-      <div id="cadastro">
+  </form>
 
-        <form method="post" action="codigo_cadastro_professor.php"> 
+</div>
 
-          <h1>Cadastro de Professores</h1> 
-           
-          <p> 
-            <label for="nome">Digite seu nome</label>
-            <input id="nome" name="nome" required="required" type="text" placeholder="nome" />
-          </p>
-           
-          <p> 
-            <label for="email">Digite Seu e-mail</label>
-            <input id="email" name="email" required="required" type="email" placeholder="contato@htmlecsspro.com"/> 
-          </p>
-          <p> 
-            <label for="matricula">Digite sua matricula</label>
-            <input id="matricula" name="matricula" required="required" type="text" placeholder="M000001" />
-          </p>
-          <p> 
-            <label for="senha">Crie uma senha</label>
-            <input id="senha" name="senha" required="required" type="password" placeholder="ex. 1234"/>
-          </p>
-
-          <p> 
-              <label for="confirmaSenha">Confirme sua senha</label>
-              <input id="confirmaSenha" name="confirmaSenha" required="required" type="password" placeholder="ex. 1234"/>
-            </p>
-           
-          <p> 
-            <input type="submit" value="Cadastrar"/> 
-          </p>
-           
-          <p class="link">  
-            Já tem conta?
-            <a href="../PHP/frmLoginProfessor.php">Entre!</a>
-          </p>
-        </form>
-      </div>
-    </div>
-  </div>  
-</body>
-</html>
+</div>
