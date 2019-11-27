8a62f876d1ba1291ce341388c4e8f1850f6df698
AlunaKeth <ke.silvacosta@gmail.com>
2019-11-27 19:20:45 -0300

    Uma parte do Perfil do Akuno, está pronta!

--- PHP/perfildoaluno.php
+++ PHP/perfildoaluno.php
@@ -1,10 +1,39 @@
 <!DOCTYPE html>
 
+<nav class="menu">
+
+<meta charset="utf-8">
+
+
+<link rel="stylesheet" href="../CSS/Menu.css">
+
+<ul>
+<li><a href="../HTML/Index.html">Home</a></li>
+	     <li><a href="formCadastrodeFuncionarios.php">Cadastros</a>
+          <ul>
+					<li><a href="formCadastrodeFuncionarios.php">Cadastro de Funcionarios</a></li>
+          <li><a href="formCadastrodeProfessores.php">Cadastro de Professores</a></li>
+          <li><a href="formCadastrodeAlunos.php">Cadastro de Alunos</a><li>
+</ul>    
+<li><a href="#">Logins</a>
+          <ul>
+					<li><a href="frmLogindeAlunos.php">Login do Aluno</a></li>
+          <li><a href="frmLoginProfessor.php">Login do Professor</a></li>
+          <li><a href="frmLoginfuncionario.php">Login do Funcionário</a><li>
+</ul>      
+
+    </ul>
+    
+  </nav>
+
 <head>
 
-  <meta charset="UTF-8" />
+  <meta charset="UTF-8"/>
 
   <title>Perfil do Aluno</title>
+
+  <h1 align ="center"> Perfil do Aluno</h1>
+
 
   <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
 
@@ -33,9 +62,9 @@ while($row = $result->fetch_assoc()){
 
 
 
-echo ("Olá $nome você é o usuário número $id.");
+echo ("Olá $nome Você é o usuário número: $id.");
 
-echo ("<BR><BR>Nome: $nome<br>E-mail: $email<br>turma: $turma<Br></a>");
+echo ("<BR><BR>Nome: $nome<br>E-mail: $email<br>Turma: $turma<Br></a>");
 
 ?>
 
