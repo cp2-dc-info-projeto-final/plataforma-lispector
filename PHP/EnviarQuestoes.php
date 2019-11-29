827a8aea6dd011baa271b2b516a6a4d1c7baf56d
AlunaKeth <ke.silvacosta@gmail.com>
2019-11-29 20:49:02 -0300

    Página de Enviar Questões pronta!

--- PHP/EnviarQuestoes.php
+++ PHP/EnviarQuestoes.php
@@ -1,4 +1,7 @@
-<link rel="stylesheet" href="../CSS/Perfis.css">
+<link rel="stylesheet" href="../CSS/Perfis.css" >
+<link rel="stylesheet" href="../CSS/PerfilDoProfessor.css" >
+
+
 <nav  class="menu">
 
 <link rel="stylesheet" href="../CSS/Menu.css">
@@ -14,52 +17,25 @@
 </ul>
 <li><a href="#">Logins</a>
           <ul>
-					<li><a href="frmLogindeAlunos.php">Login do Aluno</a></li>
+		 <li><a href="frmLogindeAlunos.php">Login do Aluno</a></li>
           <li><a href="frmLoginProfessor.php">Login do Professor</a></li>
   
   </nav>
 
+  <style>
+		
+         .conteudo1 {
 
-<div class="form">
-<form action="questoesCtrl.php" method="post">
-    <p>
-        <label for="enunciado">Enunciado</label>
-        <input type="text" name="enunciado" id="enunciado">
-    </p>
-    <p>
-        <label for="opA">Opção A:</label>
-        <input type="text" name="opA" id="opA">
-    </p>
-    <p>
-        <label for="opB">Opção B</label>
-        <input type="text" name="opB" id="opB">
-    </p>
-    <p>
-        <label for="opC">Opção C</label>
-        <input type="text" name="opC" id="op´C">
-    </p>
-    <p>
-        <label for="opD">Opção D</label>
-        <input type="text" name="opD" id="opD">
-    </p>
-    <p>
-        <label for="gabarito">Gabarito</label>
-        <input type="text"  name="gabarito" id="gabarito">
-    </p>
-    
+            border:20px solid;
+            padding: 30px;
+			background: lightskyblue;
+			
 
-    
-    <button type="submit" value="submit" class="btn btn-primary">Enviar para a Turma</button>
-  <?php
-        //session_start();
-        if(array_key_exists('erro', $_SESSION) == true){
-            $erro = $_SESSION["erro"];
-            echo "<br><b>$erro</b>";
-        }
-    ?>
-
-</form>
+        }   
+    </style>
+    </head>
+        <div class="conteudo1">
+  
+<h1> Olá, seja bem vindo ao seu espaço de envio de Arquivos. Para enviar os arquivos, clique nesse botão!
+    Enviar Arquivos<input type="file"></h1>
 </div>
-    
-</body>
-</html>
