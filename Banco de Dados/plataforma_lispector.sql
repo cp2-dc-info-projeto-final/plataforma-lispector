a43c29eb0b6ec35d2d578a04ac29b84cd07ed096
AlunaKeth <ke.silvacosta@gmail.com>
2019-12-01 06:53:34 -0300

    Acho que Consegui Criar esse Banco de Dados

--- Banco de Dados/plataforma_lispector.sql
+++ Banco de Dados/plataforma_lispector.sql
@@ -2,60 +2,61 @@ CREATE DATABASE plataforma_lispector;
      
 CREATE TABLE alunos (
 
-id int ( 60 ) NOT  NULL,
+id int ( 60 ) NOT  NULL AUTO_INCREMENT,
 nome VARCHAR ( 60 ) NOT  NULL,
 email VARCHAR ( 60 ) NOT  NULL,
 senha  VARCHAR ( 60 ) NOT  NULL,
 matricula VARCHAR ( 50 ) NOT  NULL,
 turma VARCHAR ( 5 ) NOT  NULL,
 PRIMARY KEY (ID)
-)
+);
 
 CREATE TABLE professor (
 
-id int ( 60 ) NOT  NULL,
+id int (1) NOT NULL AUTO_INCREMENT,
 nome VARCHAR ( 60 ) NOT  NULL,
 email VARCHAR ( 60 ) NOT  NULL,
 senha  VARCHAR ( 60 ) NOT  NULL,
 matricula VARCHAR ( 50 ) NOT  NULL,
 PRIMARY KEY (ID)
-)
+);
 
 CREATE TABLE tarefa (
 
-id int ( 60 ) NOT  NULL,
+id int (1) NOT  NULL AUTO_INCREMENT,
 nome VARCHAR ( 60 ) NOT  NULL,
 PRIMARY KEY (ID)
-)
+);
 
 CREATE TABLE disciplina (
 
-id int ( 60 ) NOT  NULL,
+id int ( 60 ) NOT  NULL AUTO_INCREMENT,
 nome VARCHAR ( 60 ) NOT  NULL,
 PRIMARY KEY (ID)
-)
+);
 
 CREATE TABLE questao (
 
-id int ( 60 ) NOT  NULL,
+id int ( 60 ) NOT  NULL AUTO_INCREMENT,
 pergunta VARCHAR ( 60 ) NOT  NULL,
 pergunta VARCHAR ( 60 ) NOT  NULL,
 PRIMARY KEY (ID)
-)
+);
 
 CREATE TABLE aluno_questao (
 
 id_aluno INT ( 10 ),
 id_questao INT ( 10 ),
-)
+);
 
 CREATE TABLE aluno_tarefa (
 
 id_aluno INT ( 10 ),
 id_tarefa INT ( 10 ),
+);
 
 CREATE TABLE questao_tarefa (
 
 id_tarefa INT ( 10 ),
 id_questao INT ( 10 ),
-/* Professor, eu gostaria de saber se as tabelas estão corretas e gostaria de saber onde e como eu coloco a Chave Estrangeira. Eu sei que o senhor disse que o professor Fernando vai dar orientações na segunda, mas eu quis começar a fazer para tentar adiantar. */
+);
