18369a7d71c069661b44f5e52d32398b54d07e70
AlunaKeth <ke.silvacosta@gmail.com>
2019-12-01 23:54:33 -0300

    Páginas Estilizadas

--- PHP/perfildoprofessor.php
+++ PHP/perfildoprofessor.php
@@ -1,33 +1,126 @@
-<link rel="stylesheet" href="../CSS/Perfis.css">
+<!doctype html>
+<html lang="en">
 
- <meta charset="UTF-8" />
+  <head>
+    <title>Cadastro de Professores </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <title>Perfil do Professor</title>
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700|Indie+Flower" rel="stylesheet">
+    
 
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">  
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-    <nav  class="menu">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="../css/aos.css">
 
-<link rel="stylesheet" href="../CSS/Menu.css">
+    <!-- MENU CSS -->
 
-<ul>
-<li><a href="../HTML/Index.html">Home</a></li>
-<li> <a href="perfildoprofessor.php">Seu Perfil</a><li>
-<li><a href= "../PHP/EnviarQuestoes.php">Enviar Questões</a></li>
-<<li><a href="formCadastrodeProfessores.php">Cadastros</a></li>
-          <ul>
-          <li><a href="formCadastrodeProfessores.php">Cadastro de Professores</a></li>
-          <li><a href="formCadastrodeAlunos.php">Cadastro de Alunos</a><li>
-</ul>
-<li><a href="#">Logins</a>
-          <ul>
-					<li><a href="frmLogindeAlunos.php">Login do Aluno</a></li>
-          <li><a href="frmLoginProfessor.php">Login do Professor</a></li>
-  
-  </nav>
+    <link rel="stylesheet" href="../css/style.css">
 
-  
-<?php
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
+
+    
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+
+      <header class="site-navbar site-navbar-target" role="banner">
+
+        <div class="container mb-3">
+          <div class="d-flex align-items-center">
+            <div class="site-logo mr-auto">
+              <a href="#">Plataforma Lispector<span class="text-primary">.</span></a>
+            </div>
+            <div class="site-quick-contact d-none d-lg-flex ml-auto ">
+              <div class="d-flex site-info align-items-center mr-5">
+                <span class="block-icon mr-3"><span class="icon-map-marker text-yellow"></span></span>
+              </div>
+              <div class="d-flex site-info align-items-center">
+                <span class="block-icon mr-3"><span class="icon-clock-o"></span></span>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+
+        <div class="container">
+          <div class="menu-wrap d-flex align-items-center">
+            <span class="d-inline-block d-lg-none"><a href="#" class="text-black site-menu-toggle js-menu-toggle py-5"><span class="icon-menu h3 text-black"></span></a></span>
+
+              
+
+            <nav class="site-navigation text-left mr-auto d-none d-lg-block" role="navigation">
+                <ul class="site-menu main-menu js-clone-nav mr-auto ">
+                  <li class="active"><a href="index.html" class="nav-link">Home</a></li>
+                  <li><a href="formCadastrodeAlunos.html" class="nav-link">Cadastro de Alunos</a></li>
+                  <li><a href="formCadastrodeProfessores.html" class="nav-link">Cadastro de Professores</a></li>
+                  <li><a href="formLogindeAlunos.html" class="nav-link">Login de Alunos</a></li>
+                  <li><a href="formLogindeProfessores.html" class="nav-link">Login de Professores</a></li>                 
+                </ul>
+              </nav>
+              <div class="top-social ml-auto">
+                <a href="#"><span class="icon-facebook text-teal"></span></a>
+                <a href="#"><span class="icon-twitter text-success"></span></a>
+                <a href="#"><span class="icon-linkedin text-yellow"></span></a>
+              </div>
+          </div>
+        </div>
+
+       
+
+      </header>
+
+    <div class="ftco-blocks-cover-1">
+      <div class="site-section-cover overlay" data-stellar-background-ratio="0.5" style="background-image: url('images/hero_1.jpg')">
+        <div class="container">
+          <div class="row align-items-center ">
+             
+            <div class="col-md-5 mt-5 pt-5">
+              <h1 class="mb-3 font-weight-bold text-teal"></h1>
+              <span class="text-cursive h5 text-red">Bem-vindo ao nosso site</span>
+              <h1 class="mb-3 font-weight-bold text-teal">Ciep 404 - Clarice Lispector</h1>
+            </div>
+            <div class="col-md-6 ml-auto align-self-end">
+                <img src="../IMAGENS/kid_transparent.png" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="site-section bg-light" id="contact-section">
+      <div class="container">
+        <div class="row justify-content-center text-center">
+        <div class="col-7 text-center mb-5">
+          <h2>Perfil do Aluno</h2>
+          <p>Seja muito bem-vindo(a). Este é o seu Perfil! </div>
+      </div>
+        <div class="row">
+          <div class="col-lg-8 mb-5" >
+            <form action="#" method="post">
+              <div class="form-group row">
+                <div class="col-md-6 mb-4 mb-lg-0">
+                <?php
+
 
 error_reporting(0); #oculta erros PHP
 
@@ -36,7 +129,7 @@ $email = $_GET['email'];
 
 $connection = mysqli_connect("localhost", "root", "", "plataforma_lispector");
 
-$sql = "SELECT * FROM professor WHERE email='$email'";
+$sql = "SELECT * FROM alunos WHERE email='$email'";
 
 $result = mysqli_query($connection, $sql);
 
@@ -44,15 +137,67 @@ while($row = $result->fetch_assoc()){
 
     $nome = $row['nome'];
     $id = $row['id'];
+    $turma = $row['turma'];
 }
 
-echo ("Olá $nome você é o usuário número $id.");
 
-echo ("<BR><BR>Nome: $nome<br>E-mail: $email<br></a>");
+ echo ("Olá $nome Você é o usuário número: $id."); 
+
+ echo ("<BR><BR>Nome: $nome<br>E-mail: $email<br>Turma: $turma<Br></a>"); 
 
 ?>
+
+     
+
+
+              </div>
+            </form>
+          </div>
+            </div>
+          </div>
+        </div>
       </div>
+
+
     </div>
-  </div>  
-</body>
+
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+          <h2 class="footer-heading mb-3">Obrigada !</h2>
+                <p class="mb-5">A plaraforma lispector, fica feliz em ajudar! <h2 class="footer-heading mb-4">Digite seu E-mail:</h2>
+               
+          </div>
+ 
+            </div>
+          </div>
+        </div>
+        
+
+        </div>
+      </div>
+    </footer>
+
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-migrate-3.0.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/main.js"></script>
+
+  </body>
+
 </html>
--- PHP/recuperacaosenha.php
+++ PHP/recuperacaosenha.php
@@ -1,28 +1,125 @@
-<link rel="stylesheet" href="../CSS/Logins.css" >
+<!doctype html>
+<html lang="en">
 
-<script type="text/javascript" src="../JavaScript/Logins.js"> </script>
-<div id="login">
+  <head>
+    <title>Cadastro de Professores </title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<nav  class="menu">
+    <link href="https://fonts.googleapis.com/css?family=DM+Sans:300,400,700|Indie+Flower" rel="stylesheet">
+    
 
-<link rel="stylesheet" href="../CSS/Menu.css">
+    <link rel="stylesheet" href="fonts/icomoon/style.css">
 
-<ul>
-<li><a href="../HTML/Index.html">Home</a></li>
-<li><a href="formCadastrodeProfessores.php">Cadastros</a>
-          <ul>
-          <li><a href="formCadastrodeProfessores.php">Cadastro de Professores</a></li>
-          <li><a href="formCadastrodeAlunos.php">Cadastro de Alunos</a><li>
-</ul>
-<li><a href="#">Logins</a>
-          <ul>
-					<li><a href="formLogindeAlunos.php">Login do Aluno</a></li>
-          <li><a href="frmLoginProfessor.php">Login do Professor</a></li>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="../css/jquery.fancybox.min.css">
+    <link rel="stylesheet" href="../css/owl.carousel.min.css">
+    <link rel="stylesheet" href="../css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+    <link rel="stylesheet" href="../css/aos.css">
+
+    <!-- MENU CSS -->
+
+    <link rel="stylesheet" href="../css/style.css">
+
+
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300">
 
     
-  </nav>
-  
-<?php
+    <div class="site-wrap" id="home-section">
+
+      <div class="site-mobile-menu site-navbar-target">
+        <div class="site-mobile-menu-header">
+          <div class="site-mobile-menu-close mt-3">
+            <span class="icon-close2 js-menu-toggle"></span>
+          </div>
+        </div>
+        <div class="site-mobile-menu-body"></div>
+      </div>
+
+
+
+      <header class="site-navbar site-navbar-target" role="banner">
+
+        <div class="container mb-3">
+          <div class="d-flex align-items-center">
+            <div class="site-logo mr-auto">
+              <a href="#">Plataforma Lispector<span class="text-primary">.</span></a>
+            </div>
+            <div class="site-quick-contact d-none d-lg-flex ml-auto ">
+              <div class="d-flex site-info align-items-center mr-5">
+                <span class="block-icon mr-3"><span class="icon-map-marker text-yellow"></span></span>
+              </div>
+              <div class="d-flex site-info align-items-center">
+                <span class="block-icon mr-3"><span class="icon-clock-o"></span></span>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+
+        <div class="container">
+          <div class="menu-wrap d-flex align-items-center">
+            <span class="d-inline-block d-lg-none"><a href="#" class="text-black site-menu-toggle js-menu-toggle py-5"><span class="icon-menu h3 text-black"></span></a></span>
+
+              
+
+            <nav class="site-navigation text-left mr-auto d-none d-lg-block" role="navigation">
+                <ul class="site-menu main-menu js-clone-nav mr-auto ">
+                  <li class="active"><a href="index.html" class="nav-link">Home</a></li>
+                  <li><a href="formCadastrodeAlunos.html" class="nav-link">Cadastro de Alunos</a></li>
+                  <li><a href="formCadastrodeProfessores.html" class="nav-link">Cadastro de Professores</a></li>
+                  <li><a href="formLogindeAlunos.html" class="nav-link">Login de Alunos</a></li>
+                  <li><a href="formLogindeProfessores.html" class="nav-link">Login de Professores</a></li>                 
+                </ul>
+              </nav>
+              <div class="top-social ml-auto">
+                <a href="#"><span class="icon-facebook text-teal"></span></a>
+                <a href="#"><span class="icon-twitter text-success"></span></a>
+                <a href="#"><span class="icon-linkedin text-yellow"></span></a>
+              </div>
+          </div>
+        </div>
+
+       
+
+      </header>
+
+    <div class="ftco-blocks-cover-1">
+      <div class="site-section-cover overlay" data-stellar-background-ratio="0.5" style="background-image: url('images/hero_1.jpg')">
+        <div class="container">
+          <div class="row align-items-center ">
+             
+            <div class="col-md-5 mt-5 pt-5">
+              <h1 class="mb-3 font-weight-bold text-teal"></h1>
+              <span class="text-cursive h5 text-red">Bem-vindo ao nosso site</span>
+              <h1 class="mb-3 font-weight-bold text-teal">Ciep 404 - Clarice Lispector</h1>
+            </div>
+            <div class="col-md-6 ml-auto align-self-end">
+                <img src="../IMAGENS/kid_transparent.png" alt="Image" class="img-fluid">
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="site-section bg-light" id="contact-section">
+      <div class="container">
+        <div class="row justify-content-center text-center">
+        <div class="col-7 text-center mb-5">
+          <h2>Recuperação da Senha</h2>
+          <p>Digite seu e-mail, para receber uma nova senha! </div>
+      </div>
+        <div class="row">
+          <div class="col-lg-8 mb-5" >
+            <form action="#" method="post">
+              <div class="form-group row">
+                <div class="col-md-6 mb-4 mb-lg-0">
+                <?php 
   if( !empty($_POST) ){
     // processar o pedido
     mysql_connect('localhost', 'root', 'plataforma_lispector');  // ligar à base de dados
@@ -74,3 +171,58 @@
 <?php
   }
 ?>
+
+
+
+
+              </div>
+            </form>
+          </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4">
+            <h2 class="footer-heading mb-3">Obrigada !</h2>
+                <p class="mb-5">A plaraforma lispector, fica feliz em ajudar! <h2 class="footer-heading mb-4">Digite seu E-mail:</h2>
+               
+          </div>
+ 
+            </div>
+          </div>
+        </div>
+        
+
+        </div>
+      </div>
+    </footer>
+
+    </div>
+
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/jquery-migrate-3.0.0.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/jquery.waypoints.min.js"></script>
+    <script src="js/jquery.animateNumber.min.js"></script>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/bootstrap-datepicker.min.js"></script>
+    <script src="js/aos.js"></script>
+
+    <script src="js/main.js"></script>
+
+  </body>
+
+</html>
