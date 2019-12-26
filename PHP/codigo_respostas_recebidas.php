6f7d7e013862fb5eeb6d34302bccafc8cea6ef9f
AlunaKeth <ke.silvacosta@gmail.com>
2019-12-25 21:13:29 -0300

    Código Consertado

--- PHP/codigo_respostas_recebidas.php
+++ PHP/codigo_respostas_recebidas.php
@@ -9,7 +9,7 @@ if (mysqli_connect_error($connection)) { // Retorna uma string descrevendo o ult
     echo "Erro de conexão"; // Mensagem que aparecerá, caso haja o erro.
 }
 else {
-    echo "Conexão OK!";  // Mensagem que aparecerá, caso dê certo!
+    echo "Agora que as respostas foram enviadas, acabou. Pode fechar essa janela!";  // Mensagem que aparecerá, caso dê certo!
 }
 
 $sql = "SELECT id_resposta FROM resposta";
@@ -19,7 +19,7 @@ $erro = "";
 
 if(mysqli_query($connection, $sql)){
 
-    echo '<script>alert("Respostas Enviadas");</script>'; }
+    echo '<script>alert("Respostas Enviadas. Sua nota chegará por e-mail ou o professor falará em sala de aula!");</script>'; }
  else{
     echo "ERRO: NÃO FOI POSSÍVEL CONECTAR AO BANCO DE DADOS." . mysqli_error($connection);
 }
